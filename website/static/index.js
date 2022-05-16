603beeb40f1b4c18585a61193364ad977204b479
Guy Nithikrit Raibrochareon <guy.nithi@gmail.com>
2022-05-16 20:45:25 +0700

    base.html,index.js,index.html

---
+++ website/static/index.js
@@ -0,0 +1,9 @@
+$(function() {
+            $('#test').bind('click', function() {
+                $.getJSON('/run',
+                    function(data) {
+                    // Do nothing
+                });
+                return false;
+            });
+        });
