1bb8ae4ac394d2bec0c7ad1e70e53d176187998e
Guy Nithikrit Raibrochareon <guy.nithi@gmail.com>
2022-05-21 21:03:53 +0700

    sat

--- website/main.py
+++ website/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, redirect, request, session
-from client import Client
+# from .client import Client
 
 NAME_KEY = ''
 
@@ -9,7 +9,8 @@ app.secret_key = "Mabisisthebest"
 @app.route("/login", methods=["POST","GET"])
 def login():
     if request.method == "POST":
-        pass # Not Done Yet
+        session[NAME_KEY] = request.form["name"]
+
     return render_template("login.html")
 
 
@@ -22,10 +23,10 @@ def logout():
 @app.route("/")
 @app.route("/home")
 def home():
-    # if NAME_KEY not in session:
-    #     return redirect(url_for("login"))
+    if NAME_KEY not in session:
+        return redirect(url_for("login"))
 
-    # name = session[NAME_KEY]
+    name = session[NAME_KEY]
     return render_template("index.html")
 
 
