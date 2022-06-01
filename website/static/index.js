b646ed198cface3159e59c99efa395cae89d0187
Guy Nithikrit Raibrochareon <guy.nithi@gmail.com>
2022-06-01 20:33:04 +0700

    15 more min

--- website/static/index.js
+++ website/static/index.js
@@ -1,44 +1,30 @@
 $(function() {
     $('#sendBtn').bind('click', function() {
-        var value = document.getElementById("msg").value
+        var msg = document.getElementById("msg")
+        var value = msg.value
+        msg.value = ""
         $.getJSON('/send_message',
             {val:value},
             function(data) {
 
-            });
+        });
+    });
 });
 
 
-window.addEventListener("load", function(){
+window.onload = function(){
+// window.addEventListener("load", function(){
     var update_loop = this.setInterval(update, 100);
     update()
-});
+};
 
 function update(){
     fetch('/get_messages')
-        .then(function (response) {
-            return response.text();
-        }).then(function (text) {
-            console.log('GET response text:');
-            console.log(text); // Print the greeting as text
+            .then(function (response) {
+                return response.json();
+            }).then(function (text) {
+                for (value of text["messages"]){
+                    document.write(value +"<br >");
+                }
         });
-        return false;
-}
-
-function update(){
-    fetch('/get_messages')
-    .then(function (response) {
-        return response.text();
-    }).then(function (text) {
-        console.log('GET response text:')
-        console.log(text); // Print the greeting as text
-    });
-    return false;
-}
-
-function validate(name) {
-    if(name.length >= 2){
-        return true;
-    }
-    return false;
-}
+};
